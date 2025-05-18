f7c182039d24e238b0bda12172a39de5e0286334
JhustynC <christopherarias2002@gmail.com>
2025-05-18 02:10:37 -0500

    fix: responsive menu

---
+++ app.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+
+function toggleSubMenu(button) {
+  navLinks.classList.toggle("show");
+  console.log("Menú toggled");
+}
+
+// Detectar resize y remover la clase show si está activa
+window.addEventListener("resize", () => {
+  if (navLinks.classList.contains("show")) {
+    navLinks.classList.remove("show");
+    console.log("Menú ocultado por resize");
+  }
+});
